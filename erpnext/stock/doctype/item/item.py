5a0b08cdab436f3a0c7b58859f7c77ffff6fb0d4
Rohit <tejal.s@indictranstech.com>
2015-04-01 00:43:43 +0530

    changes in item group for GetItem method Api

--- erpnext/stock/doctype/item/item.py
+++ erpnext/stock/doctype/item/item.py
@@ -43,8 +43,6 @@ class Item(WebsiteGenerator):
 		self.check_warehouse_is_set_for_stock_item()
 		self.check_stock_uom_with_bin()
 		status=check_uom_conversion(self.name)
-		print "hhhhhhhhhhhhhhhhhh"
-		frappe.errprint(status)
 		if status==False:
 			self.add_default_uom_in_conversion_factor_table()
 		elif status==True:
